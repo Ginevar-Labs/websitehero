9eb6c8dae85cff63e0691af2845c81b512e235eb
Juan Curti <jmfcdev@gmail.com>
2023-04-17 19:07:53 +0200

    Add viewport

--- components/Navbar.js
+++ components/Navbar.js
@@ -46,6 +46,7 @@ export default function Home({ origin, onGoLink }) {
         <Head>
             <title>{LocalizationTexts.META_TITLE[cookies.lang]}</title>
             <meta name="description" content={LocalizationTexts.META_DESCRIPTION[cookies.lang]}/>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
